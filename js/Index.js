a84fdf03d1bf9a462086108eaa8944b7963e243e
Diego Canelo Vigny <Dovahkiin509@gmail.com>
2021-04-29 17:51:23 -0400

    Tabla

--- js/Index.js
+++ js/Index.js
@@ -15,6 +15,58 @@ tinymce.init({
   });
   
 
+const pokemones =  [];
+const cargarTabla = () =>{
+  //Referencia de la tabla
+  let tbody = document.querySelector("#tabla-tbody");
+
+  tbody.innerHTML = "";
+  //Recorrer la lista de pokemones
+  for(let i=0; i < pokemones.length; ++i){
+    let p = pokemones[i];
+    //Por cada pokemon generar una fila (tr)
+    let tr = document.createElement("tr");
+    //Por cada atributo(Nombre,Tipo,descripcion),voy a generar celdas (td)
+    let tdNro = document.createElement("td");
+    tdNro.innerText = (i+1);
+    let tdNombre = document.createElement("td");
+    tdNombre.innerText = p.nombre;
+    let tdTipo = document.createElement("td");
+
+    let icono = document.createElement("i");
+    if (p.tipo == "fuego"){
+      //<i class="fas fa-fire"></i>
+      icono.classList.add("fas","fa-fire","text-danger","fa-3x");
+    }else if(p.tipo == "planta"){
+      //<i class="fas fa-leaf"></i>
+      icono.classList.add("fas","fa-leaf","text-success","fa-3x");
+    }else if(p.tipo == "normal"){
+      //<i class="fas fa-star"></i>
+      icono.classList.add("fas","fa-star","fa-3x");
+    }else if(p.tipo == "electrico"){
+      //<i class="fas fa-bolt"></i>
+      icono.classList.add("fas","fa-bolt","text-warning","fa-3x");
+    }else if(p.tipo == "agua"){
+      //<i class="fas fa-tint"></i>
+      icono.classList.add("fas","fa-tint","text-primary","fa-3x");
+    }
+    tdTipo.classList.add("text-center")
+    tdTipo.appendChild(icono);
+    let tdDesc = document.createElement("td");
+    tdDesc.innerHTML = p.descripcion;
+    let tdAcciones = document.createElement("td");
+
+    //Agregar las celdas al tr
+    tr.appendChild(tdNro);
+    tr.appendChild(tdNombre);
+    tr.appendChild(tdTipo);
+    tr.appendChild(tdDesc);
+    tr.appendChild(tdAcciones);
+    //Agregar el tr a la tabla
+    tbody.appendChild(tr);
+  }
+}
+
 document.querySelector("#registrar-btn").addEventListener("click", ()=>{
     //value es para obtener el valor de los input de texto
     let nombre = document.querySelector("#nombre-txt").value;
@@ -25,5 +77,16 @@ document.querySelector("#registrar-btn").addEventListener("click", ()=>{
     //El tipo se obtiene igual que los input
     let tipo = document.querySelector("#tipo-select").value;
 
-    console.log("Hola mundo!", nombre,descripcion,legendario,tipo);
+    //Como crear un objeto
+    let pokemon = {};
+    pokemon.nombre = nombre;
+    pokemon.descripcion = descripcion;
+    pokemon.tipo = tipo;
+    pokemon.legendario = legendario;
+    console.log(pokemon);
+    //Como guardar en una lista de elementos
+    pokemones.push(pokemon); // append
+    cargarTabla();
+    //Titulo, mensaje interno, (succes,info,danger)
+    Swal.fire("Exito!","Pokemon Registrado","success");
 });
